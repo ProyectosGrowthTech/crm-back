87ab4d923f1b2c41d7e6f6c7c083ca9ec0364fd2
Oscar Martin <oscaranapedro@gmail.com>
2023-04-22 18:00:21 +0200

    SWRQ-16-Create GET method to return paged invoces

--- src/main/java/org/crm/crmback/application/v1/invoice/dto/CreateInvoiceRequest.java
+++
@@ -1,9 +0,0 @@
-package org.crm.crmback.application.v1.invoice.dto;
-
-import java.math.BigDecimal;
-import java.util.Date;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
-
-public record CreateInvoiceRequest(
-    @Positive Long customerId, Date invoiceDate, BigDecimal totalAmount, @NotBlank String status) {}
