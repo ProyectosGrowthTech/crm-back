0d1b893cf4e39c63abf68b432dbebd42d8bcbb4b
Oscar Martin <oscaranapedro@gmail.com>
2023-08-10 19:05:48 +0200

    SWRQ-45-Erase-persistence-facade-in-address

--- src/main/java/org/crm/crmback/infrastructure/api/controllers/address/dto/AddressRequest.java
+++ src/main/java/org/crm/crmback/infrastructure/api/controllers/address/dto/AddressRequest.java
@@ -3,4 +3,10 @@ package org.crm.crmback.infrastructure.api.controllers.address.dto;
 import javax.validation.constraints.Positive;
 
 public record AddressRequest(
-    @Positive Long addressId, String addressLine, String city, String postalCode, String country, String addressName, String state) {}
+    @Positive Long addressId,
+    String addressLine,
+    String city,
+    String postalCode,
+    String country,
+    String addressName,
+    String state) {}
